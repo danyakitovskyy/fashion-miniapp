44147a8ff74dcee4506a300f47094b1d80418ec4
danyakitovskyy <danya.kitovskyy@gmail.com>
2025-12-19 19:05:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -16,3 +16,57 @@ buttons.forEach(btn => {
     document.getElementById(target).classList.add("active");
   });
 });
+
+const screenOrder = ["home", "eras", "persons", "archive"];
+let currentIndex = 0;
+
+function showScreen(index) {
+  if (index < 0 || index >= screenOrder.length) return;
+
+  document.querySelectorAll(".screen").forEach(s =>
+    s.classList.remove("active")
+  );
+
+  document
+    .getElementById(screenOrder[index])
+    .classList.add("active");
+
+  currentIndex = index;
+}
+
+let startX = 0;
+let endX = 0;
+
+document.addEventListener("touchstart", e => {
+  startX = e.touches[0].clientX;
+});
+
+document.addEventListener("touchend", e => {
+  endX = e.changedTouches[0].clientX;
+  handleSwipe();
+});
+
+function handleSwipe() {
+  const diff = startX - endX;
+
+  // минимальная дистанция свайпа
+  if (Math.abs(diff) < 50) return;
+
+  if (diff > 0) {
+    // свайп влево
+    showScreen(currentIndex + 1);
+  } else {
+    // свайп вправо
+    showScreen(currentIndex - 1);
+  }
+}
+
+document
+  .querySelectorAll(".bottom-nav button")
+  .forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      showScreen(index);
+    });
+  });
+
+  
